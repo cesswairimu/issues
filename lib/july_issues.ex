d598cf2044aa8748c750483b033f8392caf3de69
Cess <cessmbuguar@gmail.com>
2022-07-12 20:18:19 +0300

    initial commit

---
+++ lib/july_issues.ex
@@ -0,0 +1,18 @@
+defmodule JulyIssues do
+  @moduledoc """
+  Documentation for `JulyIssues`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JulyIssues.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/july_issues_test.exs
@@ -0,0 +1,8 @@
+defmodule JulyIssuesTest do
+  use ExUnit.Case
+  doctest JulyIssues
+
+  test "greets the world" do
+    assert JulyIssues.hello() == :world
+  end
+end
