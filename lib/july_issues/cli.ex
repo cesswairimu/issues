1c50d6e8a86a9d218bfbba543cc664fddb29e74d
Cess <cessmbuguar@gmail.com>
2022-07-14 20:28:43 +0300

    format data into a table

--- lib/july_issues/cli.ex
+++ lib/july_issues/cli.ex
@@ -1,4 +1,7 @@
 defmodule JulyIssues.CLI do
+
+  import JulyIssues.TableFormatter, only: [ print_table_for_columns: 2]
+
   @default_count 4
 
   def run(argv) do
@@ -35,9 +38,12 @@ defmodule JulyIssues.CLI do
     System.halt(0)
   end
 
-  def process({ user, project, _count }) do
+  def process({ user, project, count }) do
     JulyIssues.GithubIssues.fetch(user, project)
     |> decode_response()
+    |> sort_in_desc_order()
+    |> last(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
@@ -46,4 +52,19 @@ defmodule JulyIssues.CLI do
     IO.puts "##ERROR fetching from Github ### ==== #{error["message"]}"
     System.halt(2)
   end
+
+  def sort_in_desc_order(list) do
+    list
+    |> Enum.sort( fn arg1, arg2 ->
+       arg1["created_at"] >= arg2["created_at"] 
+      end)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse
+  end
+
+ 
 end
--- test/cli_test.exs
+++ test/cli_test.exs
@@ -2,7 +2,7 @@ defmodule CliTest do
   use ExUnit.Case
   doctest JulyIssues
 
-  import JulyIssues.CLI, only: [ parse_args: 1 ]
+  import JulyIssues.CLI, only: [ parse_args: 1, sort_in_desc_order: 1 ]
 
   test ":help returned by option parsing with -h and --help options" do
     assert parse_args(["-h",     "anything"]) == :help
@@ -15,5 +15,16 @@ defmodule CliTest do
 
   test "default count used if 2 args given" do
     assert parse_args(["user", "project_name"]) == { "user", "project_name", 4 }
+  end
+
+  test "sorts list returned in an desc order" do
+    result = sort_in_desc_order(fake_list(["c", "b", "a"]))
+    issues = for issue <- result, do: Map.get(issue, "created_at")
+    assert issues == ~w{ c b a}
+  end
+
+  defp fake_list(values) do
+    for value <- values,
+    do: %{ "created_at" => value, "other_data" => "cccc" }
   end
 end
