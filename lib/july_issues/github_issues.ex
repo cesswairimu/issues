0458b0bf9a871967234a14bc6f66c71637b923c7
Cess <cessmbuguar@gmail.com>
2022-07-15 10:53:46 +0300

    add logger and docs

--- lib/july_issues/github_issues.ex
+++ lib/july_issues/github_issues.ex
@@ -1,9 +1,13 @@
 defmodule JulyIssues.GithubIssues do
+    
+    require Logger
 
     @github_url Application.get_env(:july_issues, :github_url)
     @user_agent [{ "User-agent", "Elixir dave@progprag.com" }]
 
     def fetch(user, project) do
+        Logger.info("Fetching #{user}'s projecr #{project}")
+
         issues_url(user, project)
         |> HTTPoison.get(@user_agent)
         |> handle_response
@@ -16,6 +20,9 @@ defmodule JulyIssues.GithubIssues do
    
 
     def handle_response({ _, %{status_code: status_code, body: body} }) do
+        Logger.info("Got response: status code=#{status_code}")
+        Logger.debug( fn -> inspect(body) end )
+
         { 
             status_code |> check_for_error(),
             body        |> Poison.Parser.parse!()
--- mix.exs
+++ mix.exs
@@ -3,12 +3,13 @@ defmodule JulyIssues.MixProject do
 
   def project do
     [
-      app: :july_issues,
-      escript: escript_config(),
-      version: "0.1.0",
-      elixir: "~> 1.13",
+      app:             :july_issues,
+      escript:         escript_config(),
+      version:         "0.1.0",
+      elixir:          "~> 1.13",
+      source_url:      "https://github.com/cesswairimu/issues",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps:            deps()
     ]
   end
 
@@ -23,8 +24,10 @@ defmodule JulyIssues.MixProject do
   defp deps do
     [
       { :httpoison, "~> 1.0.0"  },
-      { :dice,      " ~> 0.0.1" },
-      { :poison,    " ~> 5.0"   }
+      { :dice,      "~> 0.0.1"  },
+      { :poison,    "~> 5.0"    },
+      { :ex_doc,     "~> 0.18.1" },
+      { :earmark,   "~> 1.2.4"  }
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
